3d82fd8fe7853518a9cc7726c9881cfcedb4cf72
Abhilash <abhilash456a@live.com>
2018-08-15 15:22:08 +0530

    15-8-18

---
+++ packt_webapp/packt_webapp/Middlewares/CustomMiddleWare.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace packt_webapp.Middlewares
+{
+    public class CustomMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        
+        public CustomMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Debug.WriteLine($" --> Request asked for {httpContext.Request.Path}");
+
+            //Call the next middleware delegate in the pipeline
+            await _next.Invoke(httpContext);
+        }
+    }
+}
