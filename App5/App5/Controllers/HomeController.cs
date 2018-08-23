cd2c3afc3052fa5579e200c356762b69654b8de4
Abhilash <abhilash456a@live.com>
2018-08-23 15:56:02 +0530

    Cookies in asp.net core

---
+++ App5/App5/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace App5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContentAccessor)
+        {
+            this._httpContextAccessor = httpContentAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            //Read cookie from IHttpContextAccessor
+            string cookieValueFromContext = _httpContextAccessor.HttpContext.Request.Cookies["x"];
+            
+            //Set the key value in Cookie
+            Set("x", "Hello from Cookie", 10);
+           
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.x = Request.Cookies["x"];
+            return View();
+        }
+
+        /// <summary>  
+        /// Get the cookie  
+        /// </summary>  
+        /// <param name="key">Key </param>  
+        /// <returns>string value</returns>  
+        public string Get(string key)
+        {
+            return Request.Cookies[key];
+        }
+        /// <summary>  
+        /// set the cookie  
+        /// </summary>  
+        /// <param name="key">key (unique indentifier)</param>  
+        /// <param name="value">value to store in cookie object</param>  
+        /// <param name="expireTime">expiration time</param>  
+        public void Set(string key, string value, int? expireTime)
+        {
+            CookieOptions option = new CookieOptions();
+            if (expireTime.HasValue)
+                option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+            else
+                option.Expires = DateTime.Now.AddMilliseconds(10);
+            Response.Cookies.Append(key, value, option);
+        }
+        /// <summary>  
+        /// Delete the key  
+        /// </summary>  
+        /// <param name="key">Key</param>  
+        public void Remove(string key)
+        {
+            Response.Cookies.Delete(key);
+        }
+    }
+}
