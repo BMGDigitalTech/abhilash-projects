95a7c03c3b746997b366d6cb9bd94a6cd05c07cb
Abhilash <abhilash456a@live.com>
2018-08-23 15:07:01 +0530

    23-8-18

---
+++ AvgApp/AvgApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AvgApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double average = 0, score = 0, currentScore = 0;
+            string input = "";
+            int counter = 0;
+            try
+            {
+                Console.WriteLine("Please enter the score and -1 to quit");
+
+                while (input != "-1")
+                {
+
+                    input = Console.ReadLine();
+                    currentScore = double.Parse(input);
+                    if (currentScore > 20)
+                    {
+                        Console.WriteLine("Please enter values below 20");
+                        continue;
+                    }
+                    if (currentScore == -1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        score += currentScore;
+                        counter++;
+                    }
+                    Console.WriteLine("Total entered : " + counter);
+                    Console.WriteLine("Total score " + score);
+                }
+
+                average = FindAverage(score, counter);
+                Console.WriteLine("Average :" + average);
+
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Score should be numeral value");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An Error occured");
+            }
+            Console.ReadLine();
+        }
+
+        public static double FindAverage(double tscore, int counter)
+        {
+            return tscore / counter;
+        }
+    }
+}
---
+++ dloop/dloop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace dloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string friends;
+            //int charlength = 0;
+            //do
+            //{
+            //    Console.WriteLine("Please enter your friends name");
+            //    friends = Console.ReadLine();
+            //    charlength += friends.Length;
+            //} while (charlength < 20);
+            //Console.WriteLine("Ok thats enough");
+            //Console.Read();
+
+            /*int counter = 0;
+            string input = "";
+            Console.WriteLine("Please enter enter to count or any key to quit");
+            
+            while (input.Equals(""))
+            {
+                input = Console.ReadLine();
+                counter++;
+                Console.WriteLine(counter);
+            }
+            Console.WriteLine("Your count is " + counter);
+            Console.Read();*/
+
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5)
+            //        break;
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    continue;
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
