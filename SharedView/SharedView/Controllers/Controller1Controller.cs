f3e3b7e2265742d7fff2c90db2e8f8c0aabe9166
Abhilash <abhilash456a@live.com>
2018-08-23 22:42:53 +0530

    23-8-18

---
+++ SharedView/SharedView/Controllers/Controller1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharedView.Controllers
+{
+    public class Controller1Controller : Controller
+    {
+        public IActionResult Contact()
+        {
+            ViewBag.location = "Bengalore";
+            return View();
+        }
+    }
+}
---
+++ SharedView/SharedView/wwwroot/js/app.js
@@ -0,0 +1,4 @@
+﻿function func() {
+    console.log("Hello ");
+}
+func();
